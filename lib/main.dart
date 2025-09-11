bef8718efd31e8fb19fcf157f9149b3a8ec16163
dianne-lab <diannemarieibajo1@gmail.com>
2025-09-11 07:14:25 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
+import 'person.dart';
 import 'teacher.dart';
 import 'student.dart';
-void main() {
-var teacher = Teacher("Mr. Smith", 40, "Male", "Mathematics");
-  teacher.introduce();
-  teacher.teach();
 
-var student = Student("Alice", 15, "Female", "Grade 10");
+void main() {
+  var teacher = Teacher("Mr. Smith", 40, "Male", "Mathematics");
+  teacher.introduce();        
+  teacher.teach();           
+  teacher.celebrateBirthday();
+  print("Teacher's age (getter): ${teacher.age}");
+
+  var student = Student("Alice", 15, "Female", "Grade 10");
   student.introduce();
   student.study();
-  student.celebrateBirthday();
-  print("Alice’s age (via getter): ${student.age}");
 }
+
 
