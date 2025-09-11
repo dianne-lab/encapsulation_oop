e9f2435b38108ed2b25d331f1cd29e03f6114e46
xandra-oop <ariannafurer@gmail.com>
2025-09-11 21:56:45 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'teacher.dart';
+import 'student.dart';
 void main() {
 var teacher = Teacher("Mr. Smith", 40, "Male", "Mathematics");
   teacher.introduce();
   teacher.teach();
+
+var student = Student("Alice", 15, "Female", "Grade 10");
+  student.introduce();
+  student.study();
+  student.celebrateBirthday();
+  print("Alice’s age (via getter): ${student.age}");
 }
+
